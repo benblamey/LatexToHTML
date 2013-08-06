87bccb197926e025c6e7603dc31b350b72b66bf2
Ben Blamey <blamey.ben@gmail.com>
2013-08-06 12:58:07 +0100

    Full path for java

--- LatexToHTML.bat
+++ LatexToHTML.bat
@@ -1,1 +1,2 @@
-java -cp C:\work\code\Ben\LatexToHTML\bin benblamey.LatexToHTML.Main
+"C:\Program Files (x86)\Java\jre7\bin\java" -cp C:\work\code\Ben\LatexToHTML\bin benblamey.LatexToHTML.Main
+pause
